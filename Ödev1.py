e0c262b4b2d2ffe12eff6bf22012069d2bc3416c
z$hr4InTheMiddle <83715370+Zehrakmnl@users.noreply.github.com>
2023-12-14 13:44:47 +0300

    Update and rename Ödev1 to Ödev1.py

--- Ödev1.py
+++ Ödev1.py
@@ -9,11 +9,16 @@ dictionary = {"Mehmet Akif":37,
               "Cahit Sitki":35,
               "Hatice Zehra":21 
               }
+
 if dictionary["Mehmet Akif"]>dictionary["Cahit Sitki"]:
+  
     if dictionary["Cahit Sitki"]> dictionary["Hatice Zehra"]:
         print("Biggest => ", dictionary["Mehmet Akif"])
+      
     elif dictionary["Cahit Sitki"]<dictionary["Hatice Zehra"]:
         print("Biggest => ", dictionary["Hatice Zehra"])
+
 elif dictionary["Mehmet Akif"]<dictionary["Cahit Sitki"]:
+  
     if dictionary["Cahit Sitki"]>dictionary["Hatice Zehra"]:
         print("Biggest => ", dictionary["Cahit Sitki"])
